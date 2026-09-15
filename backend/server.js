c2bd3bf9fb0fe8f7347eb45995f0f6675f75442c
Atiba Dee 💥 <atiba.dar12345@gmail.com>
2026-09-16 02:37:35 +0500

    add vercel.json

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,8 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import habitRoutes from "./routes/habitRoutes.js";
-import { startReminderCron } from "./cron/reminderCron.js";
 
 dotenv.config();
-connectDB();
 
 const app = express();
 
@@ -18,10 +16,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/habits", habitRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Habit Tracker API is running");
+  res.send("life log API is running");
 });
 
-startReminderCron();
+connectDB();
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
