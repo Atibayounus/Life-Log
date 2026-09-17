98f62da3f402c8fbd63dd6a1241b6d337dd0f37c
Atiba Dee 💥 <atiba.dar12345@gmail.com>
2026-09-17 16:49:42 +0500

    connecting

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -5,4 +5,15 @@ const api = axios.create({
   withCredentials: true,
 });
 
+api.interceptors.request.use((config) => {
+  const stored = localStorage.getItem("habitUser");
+  if (stored) {
+    const { token } = JSON.parse(stored);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+});
+
 export default api;
